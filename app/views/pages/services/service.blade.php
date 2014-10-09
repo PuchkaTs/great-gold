7e2ad45f3d516bb615922fffb476c8885dd35f83
Purevdorj <ncst.puje@gmail.com>
2014-10-09 15:13:40 +0800

    last commit

--- app/views/pages/services/service.blade.php
+++ app/views/pages/services/service.blade.php
@@ -4,7 +4,7 @@
 @include('layouts.partials.flexslider')
 
 <div style="min-height: 500px; width: 100%">
-    <article class="col2">
+    <article>
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 <h1 style="text-align: center">{{ $header}}</h1>
