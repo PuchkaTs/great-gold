1133a7c7019c8938db837c766858b735c5c632e6
Purevdorj <ncst.puje@gmail.com>
2014-10-28 21:20:44 +0800

    added links

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -29,9 +29,9 @@
             <h4><span class="glyphicon glyphicon-envelope"></span> Stay connected.</h4>
             <p> email: <a href="mailto:Hello@greatgoldstar.com">Hello@greatgoldstar.com</a></p>
             <br>
-            <p><a href="http://facebook.com"><i class="fa fa-facebook"></i></a>
-                <a href="http://twitter.com"><i class="fa fa-twitter"></i></a>
-                <a href="http://twitter.com"><i class="fa fa-linkedin"></i></a>
+            <p><a href="https://www.facebook.com/greatgoldstar"><i class="fa fa-facebook"></i></a>
+                <a href="https://twitter.com/GreatGoldStar"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/company/4799377?trk=vsrp_companies_cluster_name&trkInfo=VSRPsearchId%3A3555133281414502375670%2CVSRPtargetId%3A4799377%2CVSRPcmpt%3Acompanies_cluster"><i class="fa fa-linkedin"></i></a>
             </p>
         </div>
 
